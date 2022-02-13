497f07c2d4d78d83fa557f8a652a5558dfee24c2
Douglas Littig <gittil@protonmail.com>
2022-02-12 23:50:06 -0300

    Adicionando o modulo 1

---
+++ Modulo1/dataframe.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+df = pd.DataFrame({'Aluno':["José", "Carlos", "Ana", "Julia", "Debora"],
+                   'Faltas':[3,4,2,4,3],
+                   'Prova': [2,7,8,5.5,9.2],
+                   'Seminario':[8.5,5,8.2,6,9.5]})
+print(df)
---
+++ Modulo1/series.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+notas = pd.Series([10,5,7.5,9,10])
+print(notas)
+
+
+notas2 = pd.Series([10,5,7.5,9,10],index=['José', 'Carlos','André','Pedro', 'Maria'])#Atribuir Indices
+print(notas2)
+
+print(notas.describe())
+print(notas.mean())
+print(notas**2)
