61f29aaa953da696b4d908a87d5dfa151cb5bd54
Douglas <douglas.littig@gmail.com>
2022-02-14 20:02:45 -0300

    Modulo 3

--- Modulo4/operacoes_e_consultas.py
+++ Modulo4/operacoes_e_consultas.py
@@ -24,8 +24,19 @@ path = '/home/douglas/soulcode/SoulOn-Python3/Modulo4/covid_cases.csv'
 df = spark.read.format('csv')\
     .schema(schema)\
         .load(path)
-        
-df.printSchema()
 
-    
-    
+# modificando o nome de uma unica coluna
+cases = df.withColumnRenamed('infection_case','Casos de Infecçao')
+
+# modificando o nome de varias colunas
+cases1 = df.toDF(*['ID','Provincia','Cidade','Grupo',
+                   'Casos de Infecçao','Confirmado','Latitude',
+                   'Longitude'])
+#cases1.show()
+
+df2 = df.select('province','city','confirmed')
+# df2.show()
+
+df3 = df.sort(F.desc('confirmed'))
+df3.show()
+
